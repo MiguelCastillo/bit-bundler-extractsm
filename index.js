b2d84cb138c8cd214ea964fdb5c4e4f70ce83497
Miguel Castillo <mcastillo@symphono.com>
2016-11-20 13:41:59 -0500

    adding initial implementation

---
+++ index.js
@@ -0,0 +1,61 @@
+var utils = require("belty");
+var mkdirp = require("mkdirp");
+var path = require("path");
+var convertSourceMap = require("convert-source-map");
+var fs = require("fs");
+
+function extractSourcemaps(options) {
+  options = options || {};
+
+  function extractit(bundle, filename) {
+    var settings = options[bundle.name] || options;
+    settings = settings.options || settings;
+
+    var directory = path.dirname(filename);
+    var basename = path.basename(filename);
+    var sourceMapUrl = settings.sourceMapUrl || basename + ".map";
+    var sourceMapDest = filename + ".map";
+    var sourceMap = splitSourcemap(bundle);
+
+    if (sourceMap.map) {
+      mkdirp.sync(directory);
+      fs.writeFileSync(sourceMapDest, sourceMap.map);
+
+      var sourceMapComment = convertSourceMap.generateMapFileComment(sourceMapUrl);
+
+      return bundle
+        .setSourcemap(null)
+        .setContent(sourceMap.code + sourceMapComment);
+    }
+  }
+
+  function splitSourcemap(bundle) {
+    var sourceMap = bundle.sourcemap;
+    var bundleContent = bundle.content.toString();
+
+    if (!sourceMap) {
+      var converter = convertSourceMap.fromSource(bundleContent, true);
+
+      if (converter) {
+        sourceMap = converter.toJSON();
+        bundleContent = convertSourceMap.removeComments(bundleContent);
+      }
+    }
+
+    return {
+      map: sourceMap,
+      code: bundleContent
+    };
+  }
+
+  function postbundle(bundler, context) {
+    return context.visitBundles(extractit);
+  }
+
+  return {
+    postbundle: postbundle
+  }
+}
+
+
+module.exports = extractSourcemaps;
